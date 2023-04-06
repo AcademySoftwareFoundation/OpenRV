c1c7a924edcb9ee184505c58aaf9280946d52a25
Inouk Babin-Ruel <69678953+tbabiin@users.noreply.github.com>
2023-04-06 14:04:33 -0400

    100: Fixing MacOS build with CMake 3.26 (#105)
    <!--
    Thanks for your contribution! Please read this comment in its entirety.
    It's quite important.
    When a contributor merges the pull request, the title and the
    description will be used to build the merge commit!
    
    ### Pull Request TITLE
    
    It should be in the following format:
    
    [ 12345: Summary of the changes made ] Where 12345 is the corresponding
    Github Issue
    
    OR
    
    [ Summary of the changes made ] If it's solving something trivial, like
    fixing a typo.
    -->
    
    ### Linked issues
    <!--
    Link the Issue(s) this Pull Request is related to.
    
    Each PR should link to at least one issue, in the form:
    
    Use one line for each Issue. This allows auto-closing the related issue
    when the fix is merged.
    
    Fixes #12345
    Fixes #54345
    -->
    
    Fixes #100 
    
    ### Summarize your change.
    
    Fixing MacOS build error in libresample when building with CMake 3.26.
    
    ### Describe the reason for the change.
    
    The MacOS build is currently broken due to the faulty project being C
    language project and CMake Mac/darwin compiler options are mixed C/C++.
    
    ### Describe what you have tested and on which operating system.
    
    Building successfully on MacOS version Ventura 13.2.1.
    
    ### Add a list of changes, and note any that might need special
    attention during the review.
    
    I have removed standard libc++ flags as they are added by CMake
    correctly without adding them to the C compiler.
    
    ---------
    
    Signed-off-by: Inouk Babin-Ruel <inouk.babin.ruel@autodesk.com>
    Co-authored-by: Kerby Geffrard <kerby.geffrard@autodesk.com>

--- cmake/defaults/cxx_clang_defaults.cmake
+++ cmake/defaults/cxx_clang_defaults.cmake
@@ -13,30 +13,11 @@ ELSE()
   )
 ENDIF()
 
-#
-# Ref.: https://clang.llvm.org/docs/CommandGuide/clang.html
-IF(${RV_CPP_STANDARD} STREQUAL "14")
-  # Why not CMake's default gnu++14 ???
-  SET(_clang_cxx_standard
-      "c++14"
-  )
-ELSEIF(${RV_CPP_STANDARD} STREQUAL "17")
-  # Why not CMake's default gnu++17 ???
-  SET(_clang_cxx_standard
-      "c++17"
-  )
-ELSE()
-  MESSAGE(FATAL_ERROR "Unexpected RV_CPP_STANDARD: '${RV_CPP_STANDARD}'")
-ENDIF()
-
 # Common options
 ADD_COMPILE_OPTIONS(
   ${_verbose_invocation}
   -Wall
   -Wnonportable-include-path
-  -stdlib=libc++
-  # not sure we need to actually set '-std' I would assume CMake does it for us.
-  -std=${_clang_cxx_standard}
   -msse
   -msse2
   -msse3
