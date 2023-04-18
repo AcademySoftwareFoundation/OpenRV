f18061bdd42c7b6db6c6f6bb12a71eb6b32367fa
Roger Nelson <roger.nelson@autodesk.com>
2023-04-18 14:17:03 -0400

    src/build/make_pyside.py
    Signed-off-by: Roger Nelson <roger.nelson@autodesk.com>

--- src/plugins/rv-packages/otio_reader/clipHook.py
+++ src/plugins/rv-packages/otio_reader/clipHook.py
@@ -8,4 +8,26 @@ from effectHook import *
 
 # this is an example for clip that can be removed or augmented as required
 def hook_function(in_timeline, argument_map=None):
+    if isinstance(in_timeline.media_reference, otio.schema.ExternalReference):
+        related_assets_str = commands.sendInternalEvent(
+            "openassetio-get-related-references",
+            in_timeline.media_reference.target_url,
+        )
+
+        if related_assets_str:
+            media_references = {}
+
+            active_key = in_timeline.active_media_reference_key
+            media_references[active_key] = in_timeline.media_reference
+
+            for asset_pair in related_assets_str.split(';'):
+                name, location = asset_pair.split("|")
+                if name and location:
+                    media_references[name] = otio.schema.ExternalReference(
+                        target_url=location,
+                    )
+                    media_references[name] = media_reference 
+                     
+            in_timeline.set_media_references(media_references, active_key)
+
     return in_timeline
