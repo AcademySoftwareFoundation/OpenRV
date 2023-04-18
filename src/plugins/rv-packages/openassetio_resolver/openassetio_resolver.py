70e739dde16651f9e6f517ac14b0a0bc7ea7bdca
Roger Nelson <roger.nelson@autodesk.com>
2023-04-18 15:16:54 -0400

    OpenAssetIO + OTIO + OpenRV demo (#114)
    ### OpenAssetIO + OTIO + OpenRV demo
    
    ### Summarize your change.
    
    https://github.com/rogernelson/OpenRV/pull/1
    
    ### Describe the reason for the change.
    
    This is a demo in a new branch to collaborate with MovieLabs and
    OpenAssetIO. This initial commit adds a new OpenAssetIO package to
    OpenRV for resolving URIs and fetching related references. The resolve
    happens on the `incoming-source-path` event and a new event is triggered
    from the OTIO pre-clip hook to send to the OpenAssetIO package. This
    event will fetch related references on the URI in the `target_url`
    field. If there are any, the hook will add the returned references to
    the existing media_references in OTIO. In turn, these will be resolved
    into media when they are imported by the OTIO code (since it will
    trigger the `incoming-source-path` event).
    
    The OpenAssetIO package will only initialize if the
    `OPENASSETIO_DEFAULT_CONFIG` environment is set which also initializes
    the OpenAssetIO manager. So for users not using it, the package will
    have no effect.
    
    I've also added some demo-specific files in an example directory for
    running the package -- these include config files which are required to
    set up the example asset manager (BAL), an OTIO example file with a BAL
    URI, and a traits definition file (which will resolve paths based on
    environment variables). This allows the demo to be run by supplying the
    `OPENASSETIO_DEFAULT_CONFIG` environment variable and `RV_ASSET_{1,2,3}`
    to file paths on the local system.
    
    ### Describe what you have tested and on which operating system.
    
    Tested on Mac OS 13.3
    
    ---------
    
    Signed-off-by: Roger Nelson <roger.nelson@autodesk.com>

---
+++ src/plugins/rv-packages/openassetio_resolver/openassetio_resolver.py
@@ -0,0 +1,106 @@
+"""
+Resolver for openassetio assets
+"""
+# *****************************************************************************
+# Copyright (c) 2023 Autodesk, Inc.
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# *****************************************************************************
+
+import os
+import sys
+from functools import partial
+
+from rv.rvtypes import MinorMode
+
+from openassetio.log import ConsoleLogger, SeverityFilter
+from openassetio.hostApi import HostInterface, ManagerFactory
+from openassetio.pluginSystem import PythonPluginSystemManagerImplementationFactory
+
+from openassetio_mediacreation.traits.content import LocatableContentTrait
+
+
+class OpenAssetIOPlugin(MinorMode):
+    class OpenRvHost(HostInterface):
+        def identifier(self):
+            return "io.aswf.openrv"
+
+        def displayName(self):
+            return "OpenRV"
+
+
+    def __init__(self):
+        super(OpenAssetIOPlugin, self).__init__()
+        self.init(
+            "openassetio_resolver",
+            [
+                (
+                    "incoming-source-path",
+                    self.incoming_source_path,
+                    "Resolve incoming reference if its an OpenAssetIO URI",
+                ),
+                (
+                    "openassetio-get-related-references",
+                    self.get_related_references,
+                    "Return related references if there are any",
+                ),
+            ],
+
+            None,
+        )
+        self.manager = None
+
+        if os.environ.get("OPENASSETIO_DEFAULT_CONFIG"):
+            logger = SeverityFilter(ConsoleLogger())
+            impl_factory = PythonPluginSystemManagerImplementationFactory(logger)
+            host_interface = self.OpenRvHost()
+
+            self.manager = ManagerFactory.defaultManagerForInterface(host_interface, impl_factory, logger)
+
+            self.context = self.manager.createContext()
+            self.context.access = self.context.Access.kRead
+
+    @staticmethod
+    def resolve_asset(event, _, data):
+        locatable = LocatableContentTrait(data)
+        if locatable.isValid():
+            event.setReturnContent(LocatableContentTrait(data).getLocation())
+
+
+    @staticmethod
+    def fail_with_message(_, batch_element_error):
+        sys.stderr.write(f"openassetio_resolver ERROR: {batch_element_error.message}\n")
+
+
+    def incoming_source_path(self, event):
+        event.reject()
+        if not self.manager:
+            return
+        
+        in_path = event.contents().split(";")[0]
+        entity_reference = self.manager.createEntityReferenceIfValid(in_path)
+
+        if entity_reference:
+            self.manager.resolve(
+                [entity_reference],
+                {LocatableContentTrait.kId},
+                self.context,
+                partial(self.resolve_asset, event),
+                self.fail_with_message,
+            )
+
+    def get_related_references(self, event):
+        event.reject()
+        if not self.manager:
+            return
+
+        # TODO: resolve related from URI in event.contents() and 
+        # return them in a delminited string in event.setReturnContent()
+        event.setReturnContent("asset2|bal:///rv_asset2;asset3|bal:///rv_asset3")
+
+def createMode():
+    return OpenAssetIOPlugin()
+ 
+
--- src/plugins/rv-packages/otio_reader/clipHook.py
+++ src/plugins/rv-packages/otio_reader/clipHook.py
@@ -8,4 +8,25 @@ from effectHook import *
 
 # this is an example for clip that can be removed or augmented as required
 def hook_function(in_timeline, argument_map=None):
+    if isinstance(in_timeline.media_reference, otio.schema.ExternalReference):
+        related_assets_str = commands.sendInternalEvent(
+            "openassetio-get-related-references",
+            in_timeline.media_reference.target_url,
+        )
+
+        if related_assets_str:
+            media_references = in_timeline.media_references()
+
+            for asset_pair in related_assets_str.split(';'):
+                name, location = asset_pair.split("|")
+                if name and location:
+                    media_references[name] = otio.schema.ExternalReference(
+                        target_url=location,
+                    )
+
+            in_timeline.set_media_references(
+                media_references,
+                in_timeline.active_media_reference_key,
+            )
+
     return in_timeline
