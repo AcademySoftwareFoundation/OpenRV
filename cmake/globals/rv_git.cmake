4d515815175a05aa456738727dae2b92e703c061
ThomBern <thomas.bernier@autodesk.com>
2023-07-24 12:12:12 -0400

    Un-hardcoding RV Icon path

--- cmake/globals/rv_git.cmake
+++ cmake/globals/rv_git.cmake
@@ -13,18 +13,28 @@ EXECUTE_PROCESS(
   COMMAND # Should replace the 'COMMIT git log -1 --pretty=format:%h' statement used in the old Makefile
           ${_git} rev-parse --short HEAD
   RESULT_VARIABLE _result
-  OUTPUT_VARIABLE RV_GIT_COMMIT_SHORT_HASH
+  OUTPUT_VARIABLE RV_GIT_COMMIT_SHORT_HASH_RES
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
   OUTPUT_STRIP_TRAILING_WHITESPACE COMMAND_ERROR_IS_FATAL ANY
+)
+
+SET(RV_GIT_COMMIT_SHORT_HASH
+    ${RV_GIT_COMMIT_SHORT_HASH_RES}
+    CACHE STRING "RV's Commit hash"
 )
 
 EXECUTE_PROCESS(
   COMMAND # Should replace the 'COMMIT git log -1 --pretty=format:%h' statement used in the old Makefile
           ${_git} rev-parse --abbrev-ref HEAD
   RESULT_VARIABLE _result
-  OUTPUT_VARIABLE RV_GIT_BRANCH
+  OUTPUT_VARIABLE RV_GIT_BRANCH_RES
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
   OUTPUT_STRIP_TRAILING_WHITESPACE COMMAND_ERROR_IS_FATAL ANY
+)
+
+SET(RV_GIT_BRANCH
+    ${RV_GIT_BRANCH_RES}
+    CACHE STRING "RV's branch"
 )
 
 MESSAGE(STATUS "Using build branch: ${RV_GIT_BRANCH}")
