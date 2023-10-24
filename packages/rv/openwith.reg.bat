732b2862db71a6232b284c1bb39eb42a3cf3970b
Kerby Geffrard <kerby.geffrard@autodesk.com>
2023-10-24 14:06:03 -0400

    Add script variant of windows reg files for rvlink and rvpush (#302)
    <!--
    Thanks for your contribution! Please read this comment in its entirety.
    It's quite important.
    When a contributor merges the pull request, the title and the
    description will be used to build the merge commit!
    
    ### Pull Request TITLE
    
    It should be in the following format:
    
    [ 12345: Summary of the changes made ] Where 12345 is the corresponding
    Github Issue
    
    OR
    
    [ Summary of the changes made ] If it's solving something trivial, like
    fixing a typo.
    -->
    
    ### Linked issues
    <!--
    Link the Issue(s) this Pull Request is related to.
    
    Each PR should link to at least one issue, in the form:
    
    Use one line for each Issue. This allows auto-closing the related issue
    when the fix is merged.
    
    Fixes #12345
    Fixes #54345
    -->
    
    N/A
    
    ### Summarize your change.
    
    Provide a script that sets the required registry entries for rvpush and
    rvlink where the paths to RV tools are relative to the script.
    
    ### Describe the reason for the change.
    
    The current .reg files doesn't work because we can't guess the values at
    build time. Without a proper installer, we can't update the files at
    install time. The solution I propose is to use a batch script that
    computes the path to rv and to rvpush.
    
    ### Describe what you have tested and on which operating system.
    
    Tested on Windows 10 by dropping the new files in the etc folder of a
    pre-compiled OpenRV.
    
    ### Add a list of changes, and note any that might need special
    attention during the review.
    
    Please double check the entries. I *think* I did it right, but it's
    really error prone.
    
    ### If possible, provide screenshots.
    
    ---------
    
    Signed-off-by: Kerby Geffrard <kerby.geffrard@autodesk.com>

---
+++ packages/rv/openwith.reg.bat
@@ -0,0 +1,47 @@
+@echo off
+REM Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Success: Administrative privileges confirmed.
+) else (
+    echo Failure: Current permissions to execute this .BAT file are inadequate.
+    echo Please run this script as an administrator.
+    pause
+    exit /b
+)
+
+cd %~dp0
+pushd ..
+
+REM Define the path to the executable
+set "scriptDir=%cd%"
+set "rvExePath=%scriptDir%\bin\rv.exe"
+
+REM Check if the executable exists
+if not exist "%rvExePath%" (
+    echo Executable file %rvExePath% does not exist. Exiting script.
+    popd
+    pause
+    exit /b
+)
+
+@echo on
+REM Create registry keys and values
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\rv.exe" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\rv.exe" /ve /d "%rvExePath%" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\rv.exe" /v "Path" /d "%scriptDir%\bin" /f
+
+reg add "HKCR\Applications\rv.exe\shell\open" /f
+reg add "HKCR\Applications\rv.exe\shell\open" /v "FriendlyAppName" /d "RV" /f
+
+reg add "HKCR\Applications\rv.exe\shell\open\command" /f
+reg add "HKCR\Applications\rv.exe\shell\open\command" /ve /d "\"%rvExePath%\" \"%%1\"" /f
+
+reg add "HKCR\Applications\rv.exe\SupportedTypes" /f
+for %%A in (3gp aces aif aifc aiff ari au avi bmp bw cin cineon cur cut dds dpx exr gif hdr ico iff j2c j2k jp2 jpeg jpg jpt lbm lif lmp mdl mov movieproc mp4 mraysubfile openexr pbm pcd pcx pdf pgm pic png ppm psd qt rgb rgba rgbe rla rpf sgi shd sm snd stdinfb sxr targa tdl tex tga tif tiff tpic tx txr txt wal wav yuv z zfile) do (
+    reg add "HKCR\Applications\rv.exe\SupportedTypes" /v %%A /d "" /f
+)
+
+popd
+pause
+
---
+++ packages/rv/rvlink_rvpush.reg.bat
@@ -0,0 +1,43 @@
+@echo off
+REM Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Success: Administrative privileges confirmed.
+) else (
+    echo Failure: Current permissions to execute this .BAT file are inadequate.
+    echo Please run this script as an administrator.
+    pause
+    exit /b
+)
+
+cd %~dp0
+pushd ..
+
+REM Define the path to the executable
+set "scriptDir=%cd%"
+set "rvpushExePath=%scriptDir%\bin\rvpush.exe"
+
+REM Check if the executable exists
+if not exist "%rvpushExePath%" (
+    echo Executable file %rvpushExePath% does not exist. Exiting script.
+    popd
+    pause
+    exit /b
+)
+
+@echo on
+REM Create registry keys and values
+reg add "HKCR\rvlink" /f
+reg add "HKCR\rvlink" /ve /d "URL:RV Protocol" /f
+reg add "HKCR\rvlink" /v "URL Protocol" /d "" /f
+
+
+reg add "HKCR\rvlink\shell" /f
+reg add "HKCR\rvlink\shell\open" /f
+
+reg add "HKCR\rvlink\shell\open\command" /f
+reg add "HKCR\rvlink\shell\open\command" /ve /d "\"%rvpushExePath%\" \"-tag\" \"rvlink\" \"url\" \"%%1\"" /f
+
+popd
+pause
+
