3539aabdd1d01454eea75bcee6f854aeaac63697
Roger Nelson <roger.nelson@autodesk.com>
2023-11-09 18:19:08 -0500

    Adding ALEXA ARRI LogC code (#317)
    Bring back Alexa LogC and LogC Film colour transforms
    
    ### Summarize your change.
    
    The ARRI SDK was removed when RV was open-sourced since its proprietary
    code. This branch refactors the colour transforms not to use the SDK and
    puts it back into the UI.
    
    This work was started by @tbabiin but he could not complete it before
    his departure, so I picked it up where he left off.
    
    ### Describe the reason for the change.
    
    This is to restore parity with what was previously removed.
    
    ### Describe what you have tested and on which operating system.
    
    Tested on Mac (M1) 13.6.3. 
    Built on Mac 13.6.3, Windows 10, Linux Rocky 9.2 and Linux RHEL7.
    
    Signed-off-by: Roger Nelson <roger.nelson@autodesk.com>
    Co-authored-by: Inouk Babin-Ruel <inouk.babin.ruel@autodesk.com>

---
+++ src/lib/base/RVMath/RVMath/AlexaLogC.h
@@ -0,0 +1,26 @@
+//
+// Copyright (C) 2023  Autodesk, Inc. All Rights Reserved. 
+// 
+// SPDX-License-Identifier: Apache-2.0 
+//
+
+#ifndef __base__RVMath__AlexaLogC
+#define __base__RVMath__AlexaLogC
+
+#include "RVMath/LogC.h"
+
+namespace RVMath {
+
+static constexpr Param ALEXA_LOGC_PARAMS{
+  1.0f / 9.0f,               // cutPoint
+  400.0f,                    // nominalSpeed
+  0.01f,                     // midGreySignal
+  16.0f / 4095.0f,           // blackSignal
+  95.0f / 1023.0f,           // encodingBlack
+  400.0f / 1023.0f,          // encodingOffset
+  500.0f / 1023.0f * 0.525f  // encodingGain
+};
+
+}
+
+#endif
