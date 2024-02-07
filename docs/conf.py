517a394adfd6e3ececdf277f3502cb55b017deba
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2024-02-07 09:23:46 -0500

    Fixing version and release value for sphinx (#390)
    ### Fixing version and release value for sphinx
    
    ### Linked issues
    n/a
    
    ### Summarize your change.
    Fix the version and release value in the documentation
    
    ### Describe the reason for the change.
    same as above
    
    ### Describe what you have tested and on which operating system.
    n/a
    
    ### Add a list of changes, and note any that might need special
    attention during the review.
    n/a
    
    ### If possible, provide screenshots.
    
    ![image](https://github.com/AcademySoftwareFoundation/OpenRV/assets/105517825/c80a9d51-a033-44c5-92be-688b1723f660)
    
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,42 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import re
 
 project = 'Open RV'
-copyright = '2023'
 author = 'Open RV contributors'
 
-release = '1.0'
+version = '0.0'
+release = '0.0.0'
+
+# https://regex101.com/r/Sr8aQX/2
+# This pattern assumes that the SET command follows the typical syntax of a CMake SET command, 
+# with a variable name, a string value enclosed in quotes, and the CACHE STRING clause. 
+# It captures the numeric part of the value assigned to these variables.
+pattern = r'SET\(\s*(?:RV_MAJOR_VERSION|RV_MINOR_VERSION|RV_REVISION_NUMBER|RV_VERSION_YEAR)\s*"(\d+)"\s*CACHE\s*STRING\s*"[^"]+"\s*\)'
+# Open the file and read its contents
+with open('../CMakeLists.txt', 'r') as file:
+    text = file.read()
+
+# Find all matches of the pattern in the text
+matches = re.findall(pattern, text)
+
+# Extract the major, minor, and revision numbers from the matches
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+#
+# version
+# The major project version, used as the replacement for |version|. 
+# For example, for the Python documentation, this may be something like 2.6.
+#
+# release
+# The full project version, used as the replacement for |release| and e.g. in the HTML templates. 
+# For example, for the Python documentation, this may be something like 2.6.0rc1.
+#
+# If you don’t need the separation provided between version and release, just set them both to the same value.
+
+version = "%s.%s.%s" % (matches[0], matches[1], matches[2])
+release = version
+copyright = matches[3]
 
 # -- General configuration
 
