9a857130109f7043cc8a281feab8347a64639edd
Ben Chamberland <becha9260@gmail.com>
2024-07-05 15:26:01 -0400

    Create virtual environment before installing Python requirments
    Signed-off-by: Ben Chamberland <becha9260@gmail.com>

--- rvcmds.sh
+++ rvcmds.sh
@@ -73,7 +73,9 @@ RV_BUILD_PARALLELISM="${RV_BUILD_PARALLELISM:-$(python3 -c 'import os; print(os.
 
 # ALIASES: Basic commands
 
-alias rvsetup="SETUPTOOLS_USE_DISTUTILS=${SETUPTOOLS_USE_DISTUTILS} python3 -m pip install --user --upgrade -r ${RV_HOME}/requirements.txt"
+# TODO: Get whichever Python version the user has installed
+alias rvenv="python3.12 -m venv _pyenv && source _pyenv/bin/activate"
+alias rvsetup="SETUPTOOLS_USE_DISTUTILS=${SETUPTOOLS_USE_DISTUTILS} python3 -m pip install --upgrade -r ${RV_HOME}/requirements.txt"
 alias rvcfg="cmake -B ${RV_BUILD} -G \"${CMAKE_GENERATOR}\" ${CMAKE_WIN_ARCH} -DCMAKE_BUILD_TYPE=Release -DRV_DEPS_QT5_LOCATION=${QT_HOME} -DRV_DEPS_WIN_PERL_ROOT=${WIN_PERL}"
 alias rvcfgd="cmake -B ${RV_BUILD} -G \"${CMAKE_GENERATOR}\" ${CMAKE_WIN_ARCH} -DCMAKE_BUILD_TYPE=Debug -DRV_DEPS_QT5_LOCATION=${QT_HOME} -DRV_DEPS_WIN_PERL_ROOT=${WIN_PERL}"
 alias rvbuildt="cmake --build ${RV_BUILD} --config Release -v --parallel=${RV_BUILD_PARALLELISM} --target "
@@ -92,7 +94,7 @@ alias rvmkd="rvcfgd && rvbuildd"
 
 # ALIASES: Setup, Config and Build
 
-alias rvbootstrap="rvsetup && rvmk"
+alias rvbootstrap="rvenv && rvsetup && rvmk"
 alias rvbootstrapd="rvsetup && rvmkd"
 
 echo "Please ensure you have installed any required dependencies from doc/build_system/config_[os]"
