172e2be9a827fabb91f45db335060aa427b3614a
Ben Chamberland <becha9260@gmail.com>
2024-07-10 09:28:36 -0400

    Search for QT 5.15 to avoid confusion with users who have multiple QT versions
    Signed-off-by: Ben Chamberland <becha9260@gmail.com>

--- rvcmds.sh
+++ rvcmds.sh
@@ -46,11 +46,11 @@ if [ -z "$QT_HOME" ]; then
   echo "Searching for Qt installation..."
 
   if [[ "$OSTYPE" == "linux"* ]]; then
-    QT_HOME=$(find ~/Qt* -type d -maxdepth 4 -path '*/gcc_64' | head -n 1)
+    QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/gcc_64' | head -n 1)
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    QT_HOME=$(find ~/Qt* -type d -maxdepth 4 -path '*/clang_64' | head -n 1)
+    QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/clang_64' | head -n 1)
   elif [[ "$OSTYPE" == "msys"* ]]; then
-    QT_HOME=$(find c:/Qt* -type d -maxdepth 4 -path '*/msvc2019_64' | head -n 1)
+    QT_HOME=$(find c:/Qt/5.15* -type d -maxdepth 4 -path '*/msvc2019_64' | head -n 1)
   fi
 
   # Could not find Qt installation
