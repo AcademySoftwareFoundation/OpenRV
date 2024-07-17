0cda925a1d5ea287d767f36a3da85baa48c9d81f
Bernard Laberge <bernard.laberge@autodesk.com>
2024-07-17 16:18:41 -0400

    Add AJA Video Output Support to Open RV
    Signed-off-by: Bernard Laberge <bernard.laberge@autodesk.com>

---
+++ src/plugins/output/AJADevices/AJADevices/AJAModule.h
@@ -0,0 +1,53 @@
+//
+// Copyright (C) 2024  Autodesk, Inc. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <iostream>
+#include <TwkApp/VideoModule.h>
+#include <TwkGLF/GLVideoDevice.h>
+#include <TwkGLF/GL.h>
+#ifdef PLATFORM_WINDOWS
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/wglew.h>
+#endif
+
+namespace AJADevices
+{
+
+  class AJAModule : public TwkApp::VideoModule
+  {
+   public:
+    enum class OperationMode
+    {
+      ProMode,
+      SimpleMode
+    };
+
+    AJAModule( NativeDisplayPtr, unsigned int app4CC, OperationMode );
+    virtual ~AJAModule();
+
+    virtual std::string name() const;
+    virtual std::string SDKIdentifier() const;
+    virtual std::string SDKInfo() const;
+    virtual void open();
+    virtual void close();
+    virtual bool isOpen() const;
+
+    void* deviceScan() const
+    {
+      return m_devicescan;
+    }
+
+   private:
+    void* m_devicescan{ nullptr };
+    OperationMode m_mode{ OperationMode::ProMode };
+    unsigned int m_appID{ 0 };
+  };
+
+}  // namespace AJADevices
+
