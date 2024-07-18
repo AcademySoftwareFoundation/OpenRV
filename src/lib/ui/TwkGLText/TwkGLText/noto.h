1acd41af92b399521e947029b03fe3a0bf1310e5
Ben Chamberland <becha9260@gmail.com>
2024-07-18 15:01:01 -0400

    Add noto font to render mandarin characters
    Signed-off-by: Ben Chamberland <becha9260@gmail.com>

---
+++ src/lib/ui/TwkGLText/TwkGLText/noto.h
@@ -0,0 +1,14 @@
+//******************************************************************************
+// Copyright (c) 2024 Autodesk Inc. All rights reserved.
+// 
+// SPDX-License-Identifier: Apache-2.0
+// 
+//******************************************************************************
+
+#ifndef __NOTO_H__
+#define __NOTO_H__
+
+extern const unsigned char notoFont[];
+const unsigned int notoFontSize = 10560380;
+
+#endif    // End #ifdef __NOTO_H__
