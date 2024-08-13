08c7f182dc82e9e9f4d44ca04732c9ed47eadfd4
Ben Chamberland <becha9260@gmail.com>
2024-08-13 09:29:32 -0400

    Fix formatting difference between main and this branch in rvcmds.sh
    Signed-off-by: Ben Chamberland <becha9260@gmail.com>

--- rvcmds.sh
+++ rvcmds.sh
@@ -45,19 +45,19 @@ fi
 if [ -z "$QT_HOME" ]; then
   echo "Searching for Qt installation..."
 
-   if [[ "$OSTYPE" == "linux"* ]]; then
-     QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/gcc_64' | sort -V | tail -n 1)
-   elif [[ "$OSTYPE" == "darwin"* ]]; then
-     QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/macos' | sort -V | tail -n 1)
+  if [[ "$OSTYPE" == "linux"* ]]; then
+    QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/gcc_64' | sort -V | tail -n 1)
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/macos' | sort -V | tail -n 1)
 
-     # If no macos installation found, try clang_64
-     if [ -z "$QT_HOME" ]; then
-       QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/clang_64' | sort -V | tail -n 1)
-     fi
+    # If no macos installation found, try clang_64
+    if [ -z "$QT_HOME" ]; then
+      QT_HOME=$(find ~/Qt/5.15* -type d -maxdepth 4 -path '*/clang_64' | sort -V | tail -n 1)
+    fi
 
-   elif [[ "$OSTYPE" == "msys"* ]]; then
-     QT_HOME=$(find c:/Qt/5.15* -type d -maxdepth 4 -path '*/msvc2019_64' | sort -V | tail -n 1)
-   fi
+  elif [[ "$OSTYPE" == "msys"* ]]; then
+    QT_HOME=$(find c:/Qt/5.15* -type d -maxdepth 4 -path '*/msvc2019_64' | sort -V | tail -n 1)
+  fi
 
   # Could not find Qt installation
   if [ -z "$QT_HOME" ]; then
