7e1b73d395d01207a9204a5274be0727243cc37b
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2024-09-12 08:41:40 -0400

    Fix issue with venv on windows
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- rvcmds.sh
+++ rvcmds.sh
@@ -73,11 +73,16 @@ fi
 
 # Must be executed in a function as it changes the shell environment
 rvenv_shell() {
+  local activate_path=".venv/bin/activate"
+  if [[ "$OSTYPE" == "msys"* ]]; then
+    activate_path=".venv/Scripts/activate"
+  fi
+
   if [ -d ".venv" ]; then
-    source .venv/bin/activate
+    source "$activate_path"
   else
     python3 -m venv .venv
-    source .venv/bin/activate
+    source "$activate_path"
   fi
 }
 
