e2d9ccf551720f0ff209fc2fdef7a2d1be659120
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2024-09-25 15:50:29 -0400

    GLFBO fix, can now render into the default framebuffer
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- src/lib/app/RvCommon/GLView.cpp
+++ src/lib/app/RvCommon/GLView.cpp
@@ -228,7 +228,7 @@ GLView::rvGLFormat(bool stereo,
     fmt.setSwapBehavior(doubleBuffer ? QSurfaceFormat::DoubleBuffer : QSurfaceFormat::SingleBuffer);
     fmt.setStencilBufferSize(8);
     fmt.setStereo(stereo);
-
+    
     //
     //  The default value for these buffer sizes is -1, but it is
     //  illegal to set to that value so test for positive red, not
@@ -248,53 +248,6 @@ GLView::rvGLFormat(bool stereo,
     return fmt;
 }
 
-void APIENTRY glDebugOutput(GLenum source, 
-                            GLenum type, 
-                            unsigned int id, 
-                            GLenum severity, 
-                            GLsizei length, 
-                            const char *message, 
-                            const void *userParam)
-{
-    // ignore non-significant error/warning codes
-    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return; 
-
-    std::cout << "---------------" << std::endl;
-    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
-
-    switch (source)
-    {
-        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
-        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
-        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
-        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
-    } std::cout << std::endl;
-
-    switch (type)
-    {
-        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break; 
-        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
-        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
-        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
-        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
-        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
-        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
-    } std::cout << std::endl;
-    
-    switch (severity)
-    {
-        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
-        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
-        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
-        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
-    } std::cout << std::endl;
-    std::cout << std::endl;
-}
-
 void
 GLView::initializeGL()
 {
@@ -312,9 +265,6 @@ GLView::initializeGL()
         std::cout << "!!! GLView::initializeGL()-isValid()()="<<isValid()<<std::endl;
         
         initializeOpenGLFunctions();
-
-        glEnable(GL_DEBUG_OUTPUT);
-        glDebugMessageCallback(glDebugOutput, nullptr);
 
         if (m_sharedContext)
         {
@@ -449,7 +399,7 @@ GLView::paintGL()
     {
         //m_frameBuffer->makeCurrent();
         m_videoDevice->makeCurrent();
-
+        
         if (m_userActive && m_activityTimer.elapsed() > 1.0)
         {
             if (m_doc->mainPopup() && !m_doc->mainPopup()->isVisible() && hasFocus())
@@ -467,11 +417,6 @@ GLView::paintGL()
         absolutePosition(x, y);
         m_videoDevice->setAbsolutePosition(x, y);
 
-        // NOTE_QT
-        // Paint the center widget all blue.. works.
-        // There is an issue somewhere in session->render()
-        //glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-        //glClear(GL_COLOR_BUFFER_BIT);
         session->render();
 
         m_firstPaintCompleted = true;
@@ -547,7 +492,7 @@ GLView::paintGL()
         else
         {
             // NOTE_QT6: swapBuffers is under the QOpenGLContext object in Qt6.
-            context()->swapBuffers(context()->surface());
+            m_videoDevice->widget()->context()->swapBuffers(m_videoDevice->widget()->context()->surface());
         }
 
         trecord.swapEnd = session->profilingElapsedTime();
@@ -617,7 +562,7 @@ GLView::eventProcessingTimeout()
 bool
 GLView::event(QEvent* event)
 {
-    qDebug() << "Event type: " << event->type();
+    //qDebug() << "Event type: " << event->type();
 
     bool keyevent = false;
     Rv::Session* session = m_doc->session();
