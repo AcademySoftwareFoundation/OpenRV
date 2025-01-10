57e3501e40f3c45f07e249a2e23a61c0ec978bce
Éloïse Brosseau <eloise.brosseau@autodesk.com>
2025-01-10 17:30:42 -0500

    Add side by side mode
    Signed-off-by: Éloïse Brosseau <eloise.brosseau@autodesk.com>

--- src/plugins/rv-packages/otio_reader/compare_hook.py
+++ src/plugins/rv-packages/otio_reader/compare_hook.py
@@ -9,4 +9,22 @@ def hook_function(
 ) -> None:
     """A hook for the compare schema"""
     breakpoint()
-    pass
+    compare_mode = in_timeline.mode
+
+    match (compare_mode):
+        case "side_by_side":
+            orientation = in_timeline.side_by_side["orientation"]
+            gap = in_timeline.side_by_side["gap"]
+
+            all_nodes = commands.nodes()
+            layout_node = [
+                node for node in all_nodes if commands.nodeType(node) == "RVLayoutGroup"
+            ][0]
+
+            if orientation == "horizontal":
+                commands.setStringProperty(f"{layout_node}.layout.mode", ["row"])
+            else:
+                commands.setStringProperty(f"{layout_node}.layout.mode", ["column"])
+            # commands.setFloatProperty(f"{layout_node}.layout.spacing", [gap])
+
+            commands.setViewNode("defaultLayout")
