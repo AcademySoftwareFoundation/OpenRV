fb0508c325557499d2c8c7894b9313aaab8ff489
Éloïse Brosseau <eloise.brosseau@autodesk.com>
2025-02-24 15:22:22 -0500

    Add create_timeline_from_node
    Signed-off-by: Éloïse Brosseau <eloise.brosseau@autodesk.com>

--- src/plugins/rv-packages/otio_reader/otio_writer.py
+++ src/plugins/rv-packages/otio_reader/otio_writer.py
@@ -1,7 +1,7 @@
 #
-# Copyright (C) 2023  Autodesk, Inc. All Rights Reserved. 
-# 
-# SPDX-License-Identifier: Apache-2.0 
+# Copyright (C) 2025  Autodesk, Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
 #
 import math
 import numbers
@@ -49,14 +49,7 @@ TRANSITION_TYPE_MAP = {
 }
 
 
-def write_otio_file(root_node_name, file_path):
-    """
-    Create an OTIO Timeline starting from the supplied RV node and write it
-    to the path pointed to file
-    :param root_node_name: `str`
-    :param file_path: `str`
-    """
-
+def create_timeline_from_node(root_node_name):
     timeline = otio.schema.Timeline()
 
     otio_root = create_otio_from_rv_node(root_node_name, timeline=timeline)
@@ -76,6 +69,18 @@ def write_otio_file(root_node_name, file_path):
     else:
         timeline.tracks[:] = [otio_root]
 
+    return timeline
+
+
+def write_otio_file(root_node_name, file_path):
+    """
+    Create an OTIO Timeline starting from the supplied RV node and write it
+    to the path pointed to file
+    :param root_node_name: `str`
+    :param file_path: `str`
+    """
+
+    timeline = create_timeline_from_node(root_node_name)
     otio.adapters.write_to_file(timeline, file_path)
 
 
@@ -200,7 +205,7 @@ def _create_track(node_name, *args, **kwargs):
         kwargs["in_frame"] = edl_in
         kwargs["out_frame"] = edl_out
         kwargs["cut_in_frame"] = cut_in_frame
-         
+
         item = create_otio_from_rv_node(rv_node, *args, **kwargs)
 
         if has_edl:
@@ -218,7 +223,7 @@ def _create_track(node_name, *args, **kwargs):
 
         # Now that we have the items surrounding the transition, create it
         if transition:
-            kwargs["pre_item"] = track[-1] if len(track) > 0 else None,
+            kwargs["pre_item"] = (track[-1] if len(track) > 0 else None,)
             kwargs["post_item"] = item
 
             transition_node = create_otio_from_rv_node(transition, *args, **kwargs)
@@ -291,12 +296,16 @@ def _create_item(node_name, *args, **kwargs):
 
     # Create TimeRange
     start_time, duration = frames_to_rational_times(
-        start_frame=in_frame
-        if in_frame is not None
-        else get_source_start_frame(active_source_group),
-        end_frame=out_frame
-        if out_frame is not None
-        else get_source_end_frame(active_source_group),
+        start_frame=(
+            in_frame
+            if in_frame is not None
+            else get_source_start_frame(active_source_group)
+        ),
+        end_frame=(
+            out_frame
+            if out_frame is not None
+            else get_source_end_frame(active_source_group)
+        ),
         fps=fps,
     )
     if start_time or duration:
