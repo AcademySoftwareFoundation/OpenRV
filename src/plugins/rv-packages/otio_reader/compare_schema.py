d5ea18887ddeb95f6278e64485ec070b9db40dcd
Éloïse Brosseau <eloise.brosseau@autodesk.com>
2025-02-28 17:25:36 -0500

    Add is_swapped to compare schema
    Signed-off-by: Éloïse Brosseau <eloise.brosseau@autodesk.com>

--- src/plugins/rv-packages/otio_reader/compare_schema.py
+++ src/plugins/rv-packages/otio_reader/compare_schema.py
@@ -39,6 +39,7 @@ class Compare(otio.schema.Effect):
         over_with_opacity: dict | None = None,
         angular_mask: dict | None = None,
         secondary_element_id: str | None = None,
+        is_swapped: bool = False,
     ) -> None:
         super().__init__(name=name, effect_name="Compare.1")
         self.visible = visible
@@ -48,8 +49,9 @@ class Compare(otio.schema.Effect):
         self.over_with_opacity = over_with_opacity
         self.angular_mask = angular_mask
         self.secondary_element_id = secondary_element_id
+        self.is_swapped = is_swapped
 
-    _visible = otio.core.serializable_field(
+    visible = otio.core.serializable_field(
         "visible", required_type=bool, doc="visible: expects either true or false"
     )
 
@@ -127,11 +129,15 @@ class Compare(otio.schema.Effect):
     def secondary_element_id(self, val: str | None) -> None:
         self._secondary_element_id = val
 
+    is_swapped = otio.core.serializable_field(
+        "is_swapped", required_type=bool, doc="is_swapped: expects either true or false"
+    )
+
     def __str__(self) -> str:
         return (
             f"Compare({self.name}, {self.effect_name}, {self.visible}, {self.mode},"
             f" {self.side_by_side}, {self.difference}, {self.over_with_opacity},"
-            f" {self.angular_mask}, {self.secondary_element_id}"
+            f" {self.angular_mask}, {self.secondary_element_id}, {self.is_swapped}"
         )
 
     def __repr__(self) -> str:
@@ -141,5 +147,6 @@ class Compare(otio.schema.Effect):
             f" side_by_side={self.side_by_side!r}, difference={self.difference!r},"
             f" over_with_opacity={self.over_with_opacity},"
             f" angular_mask={self.angular_mask!r},"
-            f" secondary_element_id={self.secondary_element_id!r})"
+            f" secondary_element_id={self.secondary_element_id!r},"
+            f" is_swapped={self.is_swapped!r})"
         )
