fa241bb2d591baaf8dea4490532e0e28ebe3ebc5
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2025-03-12 17:00:56 -0400

    Feature/Qt6 - Fix nuke and rv integration (#715)
    ### Feature/Qt6 - Fix nuke and rv integration
    
    ### Linked issues
    n/a
    
    ### Summarize your change.
    This PR update the rvNetwork,py to Python 3 (copied from
    src/plugins/python/network/network/rvNetwork.py) and fix some issues in
    the Mu binding for the QTextStream class.
    
    I will also update the rvNetwork.py file in the main branch in another
    PR.
    
    ### Describe the reason for the change.
    Nuke and RV integration was broken. An image or video could not be
    played in RV.
    
    ### Describe what you have tested and on which operating system.
    Rocky Linux 8
    
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- src/lib/mu/MuQt6/handrolled/QTextStreamSymbols.cpp
+++ src/lib/mu/MuQt6/handrolled/QTextStreamSymbols.cpp
@@ -9,7 +9,9 @@ globalScope()->addSymbols(
                  print_void_qt_QTextStream_string, Parameters,
                  new Param(c, "textstream", "qt.QTextStream"),
                  new Param(c, "outputString", "string"), End),
+    EndArguments);
 
+addSymbols(
     new Function(c, "QTextStream", _n_QTextStream0, None, Compiled,
                  qt_QTextStream_QTextStream_QTextStream_QTextStream, Return,
                  "qt.QTextStream", Parameters,
