c90461b3ccb0255aee841bf4c80570bda71e2faf
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2025-04-09 08:38:07 -0400

    fix issue with file icons
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- src/lib/app/RvCommon/FileTypeTraits.cpp
+++ src/lib/app/RvCommon/FileTypeTraits.cpp
@@ -121,6 +121,28 @@ namespace Rv
             return provider.icon(QFileIconProvider::Folder);
         }
 
+#if defined(RV_VFX_CY2024)
+        // Adding more heuristics to find the right icon for a file based on the
+        // MIME type. The following heuristics should work for Qt 5, but in
+        // order to keep the same behavior as before, this code will only run
+        // with Qt 6 and above.
+
+        QMimeDatabase mimeDb;
+        QMimeType mime = mimeDb.mimeTypeForFile(info);
+
+        // Search for an icon based on the MIME type. (e.g. for a PNG, it would
+        // be image-png)
+        QIcon icon = QIcon::fromTheme(mime.iconName());
+        if (!icon.isNull())
+            return icon;
+
+        // Search for an icon based on the MIME generic type. (e.g. for a PNG,
+        // it would be image-x-generic)
+        icon = QIcon::fromTheme(mime.genericIconName());
+        if (!icon.isNull())
+            return icon;
+#endif
+
         return provider.icon(info);
     }
 
