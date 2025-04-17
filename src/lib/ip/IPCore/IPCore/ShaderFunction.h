baf0ee015c2de8c73b8cda588d94bd31c8c4868f
Éloïse Brosseau <54746458+eloisebrosseau@users.noreply.github.com>
2025-04-17 09:06:07 -0400

    SG-37377: Add Compare to Live Review (#738)
    ### [SG-37377](https://jira.autodesk.com/browse/SG-37377): Add Compare
    to Live Review
    
    ### Summarize your change.
    
    - [X] Add Compare OTIO schema and hook
    - [X] Add Opacity Compare mode in IPGraph and in OTIO compare hook
    - [X] Add Wipe (angular mask) mode in IPGraph and in OTIO compare hook
    - [X] Add Reverse Wipe shader to handle swapping versions
    - [X] Update requirements.txt to use the main branch of the OTIO Github
    repo to get the latest fixes for OTIO V2d before the next release is
    available
    - [X] `read_otio_string` was removed since the parsing of the OTIO
    Timeline is now down directly in the Live Review plugin
    - [X] Handle OTIO GeneratorReference to use clips with plain coloured
    background in OTIO tests
    
    ### Describe the reason for the change.
    
    In the web app, users can use four different compare modes when they are
    analyzing two versions. Since some of those compare modes were not, or
    only partially, present in RV, these PR contains all the changes
    required to add them. Opacity and Compare implementations are based on
    what is in
    [Create](https://git.autodesk.com/media-and-entertainment/shotgun-rv/pull/193).
    Note that Side by Side is entirely in the Live Review plugin in [this
    PR](https://git.autodesk.com/media-and-entertainment/rv/pull/342). The
    difference mode will need to be added in a following PR since some
    investigations need to be done to identify why the web app and RV have
    such different ways of implementing this mode.
    
    ### Describe what you have tested and on which operating system.
    
    The changes were tested on macOS during a Live Session between RV and
    the web app on
    [https://sg-create-prod.shotgunstudio.com/](https://sg-create-prod.shotgunstudio.com/).
    
    ---------
    
    Signed-off-by: Éloïse Brosseau <eloise.brosseau@autodesk.com>
    Signed-off-by: Sam.Richards@taurich.org <Sam.Richards@taurich.org>

--- src/lib/ip/IPCore/IPCore/ShaderFunction.h
+++ src/lib/ip/IPCore/IPCore/ShaderFunction.h
@@ -210,6 +210,8 @@ namespace IPCore
 
             bool usesOutputST() const { return m_usesOutputST; }
 
+            bool usesFragmentPosition() const { return m_usesFragmentPosition; }
+
             void releaseCompiledState() const;
             const Symbol::Type returnType() const;
 
@@ -280,8 +282,9 @@ namespace IPCore
             std::string m_hashString;
             ResourceUsage m_resourceUsage;
             bool m_inline;
-            bool m_usesOutputSize;
-            bool m_usesOutputST;
+            bool m_usesOutputSize{false};
+            bool m_usesOutputST{false};
+            bool m_usesFragmentPosition{false};
             bool m_usesSampling;
 
             friend class Program;
---
+++ src/lib/ip/IPCore/glsl/AngularMask.glsl
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2025 Autodesk, Inc. All Rights Reserved. 
+// 
+// SPDX-License-Identifier: Apache-2.0 
+//
+
+vec4 AngularMask(const in inputImage in0, const vec2 pivot, const float angleInRadians, const in fragmentPosition fragPos)
+{
+    // Compute a point that is 1 unit away from the pivot, on the mask line
+    vec2 pointOnLine = vec2(pivot.x + cos(angleInRadians), pivot.y + sin(angleInRadians));
+
+    // Determine if the current fragment is on the "left" side of the mask line or not
+    bool leftSide = ((pointOnLine.x - pivot.x) * (fragPos.y - pivot.y) - (pointOnLine.y - pivot.y) * (fragPos.x - pivot.x)) > 0;
+
+    if(leftSide) // not masked
+    {
+        return in0();
+    }
+    else // masked
+    {
+        // We output premultiplied fragment colors, so all zeros
+        return vec4(0.0, 0.0, 0.0, 0.0);
+    }
+}
