33f5936def6e08ea0defb9f64ce82a63dc38de5b
Éloïse Brosseau <eloise.brosseau@autodesk.com>
2025-05-27 10:15:04 -0400

    Disable hold and ghost for participant in Live Review
    Signed-off-by: Éloïse Brosseau <eloise.brosseau@autodesk.com>

--- src/lib/ip/IPCore/Session.cpp
+++ src/lib/ip/IPCore/Session.cpp
@@ -4622,6 +4622,8 @@ namespace IPCore
 
         if (m_filterLiveReviewEvents
             && (eventName == "mode-manager-toggle-mode"
+                || (eventName == "set-annotation-hold")
+                || (eventName == "set-annotation-ghost")
                 || (eventName == "remote-eval"
                     && (contents == "commands.stop()"
                         || contents
