251d20f909c30d680ad42d09a1cebf75506363cf
Michael Oliver <mcoliver@gmail.com>
2025-11-14 11:57:56 -0800

    migrate atomic_ops to cycommon
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/defaults/CYCOMMON.cmake
+++ cmake/defaults/CYCOMMON.cmake
@@ -3,6 +3,10 @@
 SET(RV_DEPS_AJA_VERSION "17.1.0")
 SET(RV_DEPS_AJA_DOWNLOAD_HASH "b9d189f77e18dbdff7c39a339b1a5dd4")
 
+# atomic_ops
+SET(RV_DEPS_ATOMIC_OPS_VERSION "7.7.0")
+SET(RV_DEPS_ATOMIC_OPS_DOWNLOAD_HASH "cc7fad1e71b3064abe1ea821ae9a9a6e")
+
 # oiio
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 SET(RV_DEPS_OIIO_VERSION "2.5.19.1")
