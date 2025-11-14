988a23534e5adfcd1c56098bf77ade89523b2b06
Michael Oliver <mcoliver@gmail.com>
2025-11-14 12:04:30 -0800

    migrate doctest to CYCOMMON
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/defaults/CYCOMMON.cmake
+++ cmake/defaults/CYCOMMON.cmake
@@ -11,6 +11,11 @@ SET(RV_DEPS_ATOMIC_OPS_DOWNLOAD_HASH "cc7fad1e71b3064abe1ea821ae9a9a6e")
 SET(RV_DEPS_DAV1D_VERSION "1.4.3")
 SET(RV_DEPS_DAV1D_DOWNLOAD_HASH "2c62106fda87a69122dc8709243a34e8")
 
+# doctest
+# https://github.com/doctest/doctest
+SET(RV_DEPS_DOCTEST_VERSION "2.4.9")
+SET(RV_DEPS_DOCTEST_DOWNLOAD_HASH "a7948b5ec1f69de6f84c7d7487aaf79b")   
+
 # oiio
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 SET(RV_DEPS_OIIO_VERSION "2.5.19.1")
