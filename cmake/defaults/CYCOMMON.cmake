ad4f33156bd8f7f54915bd7c6cf06bf8d4bee6f9
Michael Oliver <mcoliver@gmail.com>
2025-11-14 11:55:04 -0800

    moving otio to cycommon
    otio is not listed on vfxplatform.com so moving to cycommon
    
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/defaults/CYCOMMON.cmake
+++ cmake/defaults/CYCOMMON.cmake
@@ -3,3 +3,6 @@
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 SET(RV_DEPS_OIIO_VERSION "2.5.19.1")
 SET(RV_DEPS_OIIO_DOWNLOAD_HASH "5af6de5a73c6d234eed8e2874a5aed62")
+# otio
+# https://github.com/AcademySoftwareFoundation/OpenTimelineIO
+SET(RV_DEPS_OTIO_VERSION "0.18.1")
