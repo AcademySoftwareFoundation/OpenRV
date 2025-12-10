5693d82e1754deb4a7933183ad5fa3de14ce16bf
Cédrik Fuoco <105517825+cedrik-fuoco-adsk@users.noreply.github.com>
2025-12-09 22:13:45 -0500

    SG-41399 - Make sure package installed with custom python are isolated (#1019)
    ### Make sure package installed with custom python are isolated
    
    ### Linked issues
    n/a
    
    ### Summarize your change.
    1) Removed `dohq-artifactory` (not used)
    2) Added pip options (same options used in make_python.py):
       1) -s : Don't add user site-packages to sys.path
       2) -E :  Ignore all PYTHON* environment variables
       3) -I : Isolated mode
    
    ### Describe the reason for the change.
    Some issue where the custom python was seeing the site-packages from the
    system python
    
    ### Describe what you have tested and on which operating system.
    CI
    
    ### Add a list of changes, and note any that might need special
    attention during the review.
    
    ### If possible, provide screenshots.
    
    ---------
    
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- cmake/dependencies/python3.cmake
+++ cmake/dependencies/python3.cmake
@@ -281,7 +281,7 @@ SET(_requirements_install_command
     ${CMAKE_COMMAND} -E env
     ${_otio_debug_env}
     "CMAKE_ARGS=-DPYTHON_LIBRARY=${_python3_cmake_library} -DPYTHON_INCLUDE_DIR=${_include_dir} -DPYTHON_EXECUTABLE=${_python3_executable}"
-    "${_python3_executable}" -m pip install --upgrade --no-cache-dir --force-reinstall -r "${_requirements_output_file}"
+    "${_python3_executable}" -s -E -I -m pip install --upgrade --no-cache-dir --force-reinstall -r "${_requirements_output_file}"
 )
 
 IF(RV_TARGET_WINDOWS)
