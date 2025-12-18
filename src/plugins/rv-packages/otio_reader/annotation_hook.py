fe14d08ee14612a73fdb0614c8bd0ecde1ad418e
Éloïse Brosseau <54746458+eloisebrosseau@users.noreply.github.com>
2025-12-18 13:51:12 -0500

    SG-41208: Add softDeleted paint node property to support undo, redo and clear actions in Live Review (#1027)
    ### [SG-41208](https://jira.autodesk.com/browse/SG-41208): Add
    softDeleted paint node property to support undo, redo and clear actions
    in Live Review
    
    ### Summarize your change.
    
    A `softDeleted` property was added for each paint node stroke to support
    undo, redo and clear actions for OTIO-based Live Review. This new
    property is following the same pattern as the new `uuid` property. These
    aren't known by the C++ code responsible for rendering annotations, they
    are simply used to help with keeping the Annotate and the Live Review
    package synchronized in terms of the state of each annotation. For each
    undo, redo and clear action, the Annotate Mode will send an internal
    event with the least of stroke uuids affected by the action. The `name`
    field in the OTIO Paint schema was completely removed since it was
    decided it was actually needed since it isn't an OTIO effect.
    
    ### Describe the reason for the change.
    
    The `softDeleted` property was already added as a field in the OTIO
    Paint schema but it was also added as an RV node property to facilitate
    the interaction between the Live Review plugin that needs this
    information and the Annotate mode that is making the action.
    
    ### Describe what you have tested and on which operating system.
    
    Drawing, undoing, redoing and clearing strokes was tested on macOS 15.6.1.
    
    
    ---------
    
    Signed-off-by: Éloïse Brosseau <eloise.brosseau@autodesk.com>

--- src/plugins/rv-packages/otio_reader/annotation_hook.py
+++ src/plugins/rv-packages/otio_reader/annotation_hook.py
@@ -7,7 +7,6 @@
 
 
 import logging
-import uuid
 import effectHook
 import opentimelineio as otio
 from rv import commands, extra_commands
@@ -95,70 +94,65 @@ def hook_function(in_timeline: otio.schemadef.Annotation.Annotation, argument_ma
         logging.warning("Unable to set Hold and Ghost properties")
 
     for layer in in_timeline.layers:
-        if layer.name == "Paint":
-            if isinstance(layer.layer_range, otio.opentime.TimeRange):
-                time_range = layer.layer_range
-            else:
-                time_range = otio.opentime.TimeRange(layer.layer_range["start_time"], layer.layer_range["duration"])
+        if isinstance(layer.layer_range, otio.opentime.TimeRange):
+            time_range = layer.layer_range
+        else:
+            time_range = otio.opentime.TimeRange(layer.layer_range["start_time"], layer.layer_range["duration"])
 
-            relative_time = time_range.end_time_inclusive()
-            frame = relative_time.to_frames()
+        relative_time = time_range.end_time_inclusive()
+        frame = relative_time.to_frames()
 
-            source_node = argument_map.get("source_group")
-            paint_node = extra_commands.nodesInGroupOfType(source_node, "RVPaint")[0]
-            paint_component = f"{paint_node}.paint"
-            stroke_id = commands.getIntProperty(f"{paint_component}.nextId")[0] + 1
-            pen_component = f"{paint_node}.pen:{stroke_id}:{frame}:annotation"
-            frame_component = f"{paint_node}.frame:{frame}"
+        source_node = argument_map.get("source_group")
+        paint_node = extra_commands.nodesInGroupOfType(source_node, "RVPaint")[0]
+        paint_component = f"{paint_node}.paint"
+        stroke_id = commands.getIntProperty(f"{paint_component}.nextId")[0] + 1
+        pen_component = f"{paint_node}.pen:{stroke_id}:{frame}:annotation"
+        frame_component = f"{paint_node}.frame:{frame}"
 
-            # Set properties on the paint component of the RVPaint node
-            effectHook.set_rv_effect_props(paint_component, {"nextId": stroke_id})
+        # Set properties on the paint component of the RVPaint node
+        effectHook.set_rv_effect_props(paint_component, {"nextId": stroke_id})
 
-            start_time = int(time_range.start_time.value)
-            end_time = int(start_time + time_range.duration.value)
+        start_time = int(time_range.start_time.value)
+        end_time = int(start_time + time_range.duration.value)
 
-            duration = end_time - start_time
+        duration = end_time - start_time
 
-            # Add and set properties on the pen component of the RVPaint node
-            effectHook.add_rv_effect_props(
-                pen_component,
-                {
-                    "color": list(map(float, layer.rgba)),
-                    "brush": layer.brush,
-                    "debug": 1,
-                    "join": 3,
-                    "cap": 1,
-                    "splat": 0,
-                    "mode": 0 if layer.type == "COLOR" else 1,
-                    "startFrame": start_time,
-                    "duration": duration,
-                    "uuid": layer.id
-                    if layer.id
-                    else str(uuid.uuid4()),  # TODO: Remove when CR starts sending this property
-                },
+        # Add and set properties on the pen component of the RVPaint node
+        effectHook.add_rv_effect_props(
+            pen_component,
+            {
+                "color": list(map(float, layer.rgba)),
+                "brush": layer.brush,
+                "debug": 1,
+                "join": 3,
+                "cap": 1,
+                "splat": 0,
+                "mode": 0 if layer.type == "COLOR" else 1,
+                "startFrame": start_time,
+                "duration": duration,
+                "uuid": layer.id,
+            },
+        )
+
+        points_property = f"{pen_component}.points"
+        width_property = f"{pen_component}.width"
+
+        if not commands.propertyExists(points_property):
+            commands.newProperty(points_property, commands.FloatType, 2)
+        if not commands.propertyExists(width_property):
+            commands.newProperty(width_property, commands.FloatType, 1)
+
+        for point in layer.points:
+            world_coordinate_x, world_coordinate_y, world_coordinate_width = _transform_otio_to_world_coordinate(point)
+
+            commands.insertFloatProperty(
+                points_property,
+                [world_coordinate_x, world_coordinate_y],
             )
+            commands.insertFloatProperty(width_property, [world_coordinate_width])
 
-            points_property = f"{pen_component}.points"
-            width_property = f"{pen_component}.width"
+        if not layer.soft_deleted:
+            if not commands.propertyExists(f"{frame_component}.order"):
+                commands.newProperty(f"{frame_component}.order", commands.StringType, 1)
 
-            if not commands.propertyExists(points_property):
-                commands.newProperty(points_property, commands.FloatType, 2)
-            if not commands.propertyExists(width_property):
-                commands.newProperty(width_property, commands.FloatType, 1)
-
-            for point in layer.points:
-                world_coordinate_x, world_coordinate_y, world_coordinate_width = _transform_otio_to_world_coordinate(
-                    point
-                )
-
-                commands.insertFloatProperty(
-                    points_property,
-                    [world_coordinate_x, world_coordinate_y],
-                )
-                commands.insertFloatProperty(width_property, [world_coordinate_width])
-
-            if not layer.soft_deleted:
-                if not commands.propertyExists(f"{frame_component}.order"):
-                    commands.newProperty(f"{frame_component}.order", commands.StringType, 1)
-
-                commands.insertStringProperty(f"{frame_component}.order", [f"pen:{stroke_id}:{frame}:annotation"])
+            commands.insertStringProperty(f"{frame_component}.order", [f"pen:{stroke_id}:{frame}:annotation"])
