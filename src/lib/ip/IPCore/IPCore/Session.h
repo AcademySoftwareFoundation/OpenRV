70d10d44adb97e65580f8e6d8889c4cfca325daf
Roger Nelson <roger.nelson@autodesk.com>
2026-02-04 18:12:34 -0500

    Fix crashes when calling sourcesAtFrame when clearing
    Signed-off-by: Roger Nelson <roger.nelson@autodesk.com>

--- src/lib/ip/IPCore/IPCore/Session.h
+++ src/lib/ip/IPCore/IPCore/Session.h
@@ -1089,6 +1089,7 @@ namespace IPCore
         void waitForUploadToFinish();
 
         bool beingDeleted() { return m_beingDeleted; }
+        bool beingCleared() { return m_beingCleared; }
 
         void physicalDeviceChanged(const VideoDevice*);
 
@@ -1256,6 +1257,7 @@ namespace IPCore
         bool m_audioUnavailble;
         static float m_audioDrift;
         bool m_beingDeleted;
+        bool m_beingCleared{false};
         CacheStats m_cacheStats;
         Time m_syncOffset;
         SyncTimes m_syncTimes;
