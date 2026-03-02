7c5b1cb89132992c9f0a030fc619bff485c3103d
chenl1 <ling.jie.chen@autodesk.com>
2026-03-02 16:29:13 -0500

    Remove safeguard causing runtime error when clearing loaded media
    Signed-off-by: chenl1 <ling.jie.chen@autodesk.com>

--- src/lib/ip/IPMu/CommandsModule.cpp
+++ src/lib/ip/IPMu/CommandsModule.cpp
@@ -3895,11 +3895,6 @@ namespace IPMu
         if (!node)
             throwBadArgumentException(NODE_THIS, NODE_THREAD, "nil node name");
 
-        if (auto val = returnIfGraphUnsafe(s, Pointer(0)))
-        {
-            NODE_RETURN(*val);
-        }
-
         if (IPNode* n = s->graph().findNode(node->c_str()))
         {
             type = stype->allocate(n->protocol());
@@ -4125,11 +4120,6 @@ namespace IPMu
         if (!name)
         {
             throwBadArgumentException(NODE_THIS, NODE_THREAD, "no node name specified");
-        }
-
-        if (auto val = returnIfGraphUnsafe(s, Pointer(0)))
-        {
-            NODE_RETURN(*val);
         }
 
         if (IPNode* node = s->graph().findNode(name->c_str()))
