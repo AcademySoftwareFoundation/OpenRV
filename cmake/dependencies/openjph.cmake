f5b29cb8508f06c020c1348fb657b22f5b10a456
Michael Oliver <mcoliver@gmail.com>
2026-03-02 19:08:34 -0800

    fix lib name Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/dependencies/openjph.cmake
+++ cmake/dependencies/openjph.cmake
@@ -37,7 +37,7 @@ ENDIF()
 
 IF(RV_TARGET_WINDOWS)
   RV_MAKE_STANDARD_LIB_NAME("openjph.${_version_major}.${_version_minor}${RV_OPENJPH_DEBUG_POSTFIX}" "${RV_DEPS_OPENJPH_VERSION}" "SHARED" "")
-  SET(_libname
+  SET(_implibname
       "openjph.${_version_major}.${_version_minor}${RV_OPENJPH_DEBUG_POSTFIX}.lib"
   )
   SET(_implibpath
