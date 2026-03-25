28569482976321f2e8f881356faf6db100825a5a
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2026-03-25 11:35:46 -0400

    build: add LibRaw find_package dispatcher
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

---
+++ cmake/dependencies/build/raw.cmake
@@ -0,0 +1,81 @@
+#
+# Copyright (C) 2022  Autodesk, Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+IF(RV_TARGET_WINDOWS)
+  EXTERNALPROJECT_ADD(
+    ${_target}
+    URL ${_download_url}
+    URL_MD5 ${_download_hash}
+    DOWNLOAD_NAME ${_target}_${_version}.tar.gz
+    DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    SOURCE_DIR ${_base_dir}/src
+    INSTALL_DIR ${_install_dir}
+    DEPENDS ZLIB::ZLIB
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND nmake /f Makefile.msvc
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE TRUE
+    BUILD_ALWAYS FALSE
+    BUILD_BYPRODUCTS ${_byproducts}
+    USES_TERMINAL_BUILD TRUE
+  )
+
+  # Building with nmake for Windows doesn't provide a nice install target, we need to do it manually. We remove unneeded files after copying the required
+  # directories
+  ADD_CUSTOM_COMMAND(
+    TARGET ${_target}
+    POST_BUILD
+    COMMENT "Installing ${_target}'s libs & files into ${_install_dir}"
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${_base_dir}/src/lib ${_lib_dir}
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${_base_dir}/src/libraw ${_include_dir}/libraw
+    # Copy only the DLL on Windows because there is no option to disable the "examples/samples" with Makefile.msvc.
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${_bin_dir}
+    COMMAND ${CMAKE_COMMAND} -E copy ${_base_dir}/src/bin/${_libname} ${_bin_dir}
+    COMMAND ${CMAKE_COMMAND} -E rm ${_lib_dir}/Makefile
+  )
+
+ELSE()
+  # The '_configure_options' list gets reset and initialized in 'RV_CREATE_STANDARD_DEPS_VARIABLES'
+  SET(_configure_options
+      ""
+  ) # Overrides defaults set in 'RV_CREATE_STANDARD_DEPS_VARIABLES'
+  LIST(APPEND _configure_options "--prefix=${_install_dir}")
+  LIST(APPEND _configure_options "--enable-lcms")
+
+  GET_TARGET_PROPERTY(_lcms_include_dir lcms INTERFACE_INCLUDE_DIRECTORIES)
+  SET(_lcms2_flags
+      "-I${_lcms_include_dir}"
+  )
+  SET(_lcms2_libs
+      "-L${RV_STAGE_LIB_DIR} -llcms"
+  )
+
+  SET(_configure_command
+      ${CMAKE_COMMAND} -E env LCMS2_CFLAGS='${_lcms2_flags}' ${CMAKE_COMMAND} -E env LCMS2_LIBS='${_lcms2_libs}' ${_configure_command}
+  )
+
+  EXTERNALPROJECT_ADD(
+    ${_target}
+    URL ${_download_url}
+    URL_MD5 ${_download_hash}
+    DOWNLOAD_NAME ${_target}_${_version}.tar.gz
+    DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    SOURCE_DIR ${_source_dir}
+    INSTALL_DIR ${_install_dir}
+    DEPENDS ZLIB::ZLIB lcms
+    CONFIGURE_COMMAND aclocal
+    COMMAND autoreconf --install
+    COMMAND ${_configure_command} ${_configure_options}
+    BUILD_COMMAND ${_make_command} -j${_cpu_count}
+    INSTALL_COMMAND ${_make_command} install
+    BUILD_IN_SOURCE TRUE
+    BUILD_ALWAYS FALSE
+    BUILD_BYPRODUCTS ${_byproducts}
+    USES_TERMINAL_BUILD TRUE
+  )
+ENDIF()
--- cmake/dependencies/raw.cmake
+++ cmake/dependencies/raw.cmake
@@ -11,17 +11,29 @@
 
 RV_CREATE_STANDARD_DEPS_VARIABLES("RV_DEPS_RAW" "${RV_DEPS_RAW_VERSION}" "make" "../src/configure")
 IF(RV_TARGET_LINUX)
-  # Overriding _lib_dir created in 'RV_CREATE_STANDARD_DEPS_VARIABLES' since this CMake-based project isn't using lib64
+  # Overriding _lib_dir created in 'RV_CREATE_STANDARD_DEPS_VARIABLES' since this autotools-based project isn't using lib64
   SET(_lib_dir
       ${_install_dir}/lib
   )
 ENDIF()
-RV_SHOW_STANDARD_DEPS_VARIABLES()
+
+# LibRaw uses autotools (not CMake), so no CONFIG files. Fall back to pkg-config.
+RV_FIND_DEPENDENCY(
+  TARGET
+  ${_target}
+  PACKAGE
+  LibRaw
+  VERSION
+  ${_version}
+  PKG_CONFIG_NAME
+  libraw
+  DEPS_LIST_TARGETS
+  libraw::raw
+)
 
 SET(_download_url
     "https://github.com/LibRaw/LibRaw/archive/refs/tags/${_version}.tar.gz"
 )
-
 SET(_download_hash
     "${RV_DEPS_RAW_DOWNLOAD_HASH}"
 )
@@ -36,98 +48,67 @@ ELSE()
   RV_MAKE_STANDARD_LIB_NAME("libraw" "${_libraw_lib_version}" "SHARED" "")
 ENDIF()
 
-IF(RV_TARGET_WINDOWS)
-  EXTERNALPROJECT_ADD(
+IF(NOT ${_target}_FOUND)
+  INCLUDE(${CMAKE_CURRENT_LIST_DIR}/build/raw.cmake)
+
+  RV_STAGE_DEPENDENCY_LIBS(TARGET ${_target} LIBNAME ${_libname})
+
+  RV_ADD_IMPORTED_LIBRARY(
+    NAME
+    libraw::raw
+    TYPE
+    SHARED
+    LOCATION
+    ${_libpath}
+    SONAME
+    ${_libname}
+    IMPLIB
+    ${_implibpath}
+    INCLUDE_DIRS
+    ${_include_dir}
+    DEPENDS
     ${_target}
-    URL ${_download_url}
-    URL_MD5 ${_download_hash}
-    DOWNLOAD_NAME ${_target}_${_version}.tar.gz
-    DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    SOURCE_DIR ${_base_dir}/src
-    INSTALL_DIR ${_install_dir}
-    DEPENDS ZLIB::ZLIB
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND nmake /f Makefile.msvc
-    INSTALL_COMMAND ""
-    BUILD_IN_SOURCE TRUE
-    BUILD_ALWAYS FALSE
-    BUILD_BYPRODUCTS ${_byproducts}
-    USES_TERMINAL_BUILD TRUE
+    ADD_TO_DEPS_LIST
   )
-
-  # Building with nmake for Windows doesn't provide a nice install target, we need to do it manually. We remove unneeded files after copying the required
-  # directories
-  ADD_CUSTOM_COMMAND(
-    TARGET ${_target}
-    POST_BUILD
-    COMMENT "Installing ${_target}'s libs & files into ${_install_dir}"
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${_base_dir}/src/lib ${_lib_dir}
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${_base_dir}/src/libraw ${_include_dir}/libraw
-    # Copy only the DLL on Windows because there is no option to disable the "examples/samples" with Makefile.msvc.
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${_bin_dir}
-    COMMAND ${CMAKE_COMMAND} -E copy ${_base_dir}/src/bin/${_libname} ${_bin_dir}
-    COMMAND ${CMAKE_COMMAND} -E rm ${_lib_dir}/Makefile
-  )
-
 ELSE()
-  # The '_configure_options' list gets reset and initialized in 'RV_CREATE_STANDARD_DEPS_VARIABLES'
-  SET(_configure_options
+  # libraw's .pc file sets Cflags to "-I.../include/libraw -I.../include". pkg-config picks up the first (subdirectory) but IOraw.cpp uses #include
+  # <libraw/libraw.h> which needs the parent. Override _include_dir to the install root's include dir.
+  GET_FILENAME_COMPONENT(_include_dir "${_install_dir}/include" ABSOLUTE)
+
+  # pkg-config found path: create the imported target. pkg-config creates PkgConfig::libraw, not libraw::raw. Resolve the actual library file from pkg-config
+  # (don't use _libname which has the hardcoded build-from-source version).
+  SET(_raw_found_lib
       ""
-  ) # Overrides defaults set in 'RV_CREATE_STANDARD_DEPS_VARIABLES'
-  LIST(APPEND _configure_options "--prefix=${_install_dir}")
-  LIST(APPEND _configure_options "--enable-lcms")
+  )
+  IF(${_target}_PC_LINK_LIBRARIES)
+    LIST(GET ${_target}_PC_LINK_LIBRARIES 0 _raw_found_lib)
+  ENDIF()
+  IF(NOT _raw_found_lib
+     OR NOT EXISTS "${_raw_found_lib}"
+  )
+    # Fallback: find the library in _lib_dir
+    FILE(GLOB _raw_found_libs "${_lib_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}raw${CMAKE_SHARED_LIBRARY_SUFFIX}"
+         "${_lib_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}raw.*${CMAKE_SHARED_LIBRARY_SUFFIX}*"
+    )
+    IF(_raw_found_libs)
+      LIST(GET _raw_found_libs 0 _raw_found_lib)
+    ENDIF()
+  ENDIF()
 
-  GET_TARGET_PROPERTY(_lcms_include_dir lcms INTERFACE_INCLUDE_DIRECTORIES)
-  SET(_lcms2_flags
-      "-I${_lcms_include_dir}"
-  )
-  SET(_lcms2_libs
-      "-L${RV_STAGE_LIB_DIR} -llcms"
-  )
+  IF(NOT TARGET libraw::raw)
+    RV_ADD_IMPORTED_LIBRARY(
+      NAME
+      libraw::raw
+      TYPE
+      SHARED
+      LOCATION
+      ${_raw_found_lib}
+      INCLUDE_DIRS
+      ${_include_dir}
+      DEPENDS
+      ${_target}
+    )
+  ENDIF()
 
-  SET(_configure_command
-      ${CMAKE_COMMAND} -E env LCMS2_CFLAGS='${_lcms2_flags}' ${CMAKE_COMMAND} -E env LCMS2_LIBS='${_lcms2_libs}' ${_configure_command}
-  )
-
-  EXTERNALPROJECT_ADD(
-    ${_target}
-    URL ${_download_url}
-    URL_MD5 ${_download_hash}
-    DOWNLOAD_NAME ${_target}_${_version}.tar.gz
-    DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    SOURCE_DIR ${_source_dir}
-    INSTALL_DIR ${_install_dir}
-    DEPENDS ZLIB::ZLIB lcms
-    CONFIGURE_COMMAND aclocal
-    COMMAND autoreconf --install
-    COMMAND ${_configure_command} ${_configure_options}
-    BUILD_COMMAND ${_make_command} -j${_cpu_count}
-    INSTALL_COMMAND ${_make_command} install
-    BUILD_IN_SOURCE TRUE
-    BUILD_ALWAYS FALSE
-    BUILD_BYPRODUCTS ${_byproducts}
-    USES_TERMINAL_BUILD TRUE
-  )
+  RV_STAGE_DEPENDENCY_LIBS(TARGET ${_target} TARGET_LIBS libraw::raw)
 ENDIF()
-
-RV_STAGE_DEPENDENCY_LIBS(TARGET ${_target} LIBNAME ${_libname})
-
-RV_ADD_IMPORTED_LIBRARY(
-  NAME
-  LibRaw::raw
-  TYPE
-  SHARED
-  LOCATION
-  ${_libpath}
-  SONAME
-  ${_libname}
-  IMPLIB
-  ${_implibpath}
-  INCLUDE_DIRS
-  ${_include_dir}
-  DEPENDS
-  ${_target}
-  ADD_TO_DEPS_LIST
-)
