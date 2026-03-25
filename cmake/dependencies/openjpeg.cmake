910e27ad19f29584769435cf7e6ec3b9c8e6b4f8
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2026-03-25 12:04:16 -0400

    build: add OpenJPEG find_package dispatcher
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- cmake/dependencies/openjpeg.cmake
+++ cmake/dependencies/openjpeg.cmake
@@ -19,7 +19,20 @@ IF(RV_TARGET_LINUX)
       ${_install_dir}/lib
   )
 ENDIF()
-RV_SHOW_STANDARD_DEPS_VARIABLES()
+
+# OpenJPEG ships CMake CONFIG files (OpenJPEGConfig.cmake). CONFIG creates target `openjp2` (not namespaced). Fall back to pkg-config.
+RV_FIND_DEPENDENCY(
+  TARGET
+  ${_target}
+  PACKAGE
+  OpenJPEG
+  VERSION
+  ${_version}
+  PKG_CONFIG_NAME
+  libopenjp2
+  DEPS_LIST_TARGETS
+  openjp2
+)
 
 SET(_download_url
     "https://github.com/uclouvain/openjpeg/archive/refs/tags/v${_version}.tar.gz"
@@ -28,76 +41,67 @@ SET(_download_hash
     ${RV_DEPS_OPENJPEG_DOWNLOAD_HASH}
 )
 
+# Shared naming logic (used by both build and found paths)
 IF(RV_TARGET_WINDOWS)
   RV_MAKE_STANDARD_LIB_NAME("openjp2" "" "SHARED" "")
 ELSE()
   RV_MAKE_STANDARD_LIB_NAME("openjp2" "${RV_DEPS_OPENJPEG_VERSION}" "SHARED" "")
 ENDIF()
 
-# The '_configure_options' list gets reset and initialized in 'RV_CREATE_STANDARD_DEPS_VARIABLES'
-
-# Do not build the executables (OpenJPEG calls them "codec executables"). BUILD_THIRDPARTY options is valid only if BUILD_CODEC=ON. PNG, TIFF and ZLIB are not
-# needed anymore because they are used for the executables only.
-LIST(APPEND _configure_options "-DBUILD_CODEC=OFF")
-
-EXTERNALPROJECT_ADD(
-  ${_target}
-  URL ${_download_url}
-  URL_MD5 ${_download_hash}
-  DOWNLOAD_NAME ${_target}_${_version}.tar.gz
-  DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
-  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-  SOURCE_DIR ${_source_dir}
-  BINARY_DIR ${_build_dir}
-  INSTALL_DIR ${_install_dir}
-  DEPENDS ZLIB::ZLIB TIFF::TIFF PNG::PNG
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} ${_configure_options}
-  BUILD_COMMAND ${_cmake_build_command}
-  INSTALL_COMMAND ${_cmake_install_command}
-  BUILD_IN_SOURCE FALSE
-  BUILD_ALWAYS FALSE
-  BUILD_BYPRODUCTS ${_byproducts}
-  USES_TERMINAL_BUILD TRUE
-)
-
-RV_STAGE_DEPENDENCY_LIBS(TARGET ${_target} LIBNAME ${_libname})
-
 SET(_openjpeg_include_dir
     "${_include_dir}/openjpeg-${_version_major}.${_version_minor}"
 )
 
-IF(RV_TARGET_WINDOWS)
-  RV_ADD_IMPORTED_LIBRARY(
-    NAME
-    OpenJpeg::OpenJpeg
-    TYPE
-    SHARED
-    LOCATION
-    ${_libpath}
-    SONAME
-    ${_libname}
-    IMPLIB
-    ${_bin_dir}/${_implibname}
-    INCLUDE_DIRS
-    ${_openjpeg_include_dir}
-    DEPENDS
-    ${_target}
-    ADD_TO_DEPS_LIST
-  )
+IF(NOT ${_target}_FOUND)
+  INCLUDE(${CMAKE_CURRENT_LIST_DIR}/build/openjpeg.cmake)
+
+  RV_STAGE_DEPENDENCY_LIBS(TARGET ${_target} LIBNAME ${_libname})
+
+  IF(RV_TARGET_WINDOWS)
+    RV_ADD_IMPORTED_LIBRARY(
+      NAME
+      OpenJpeg::OpenJpeg
+      TYPE
+      SHARED
+      LOCATION
+      ${_libpath}
+      SONAME
+      ${_libname}
+      IMPLIB
+      ${_bin_dir}/${_implibname}
+      INCLUDE_DIRS
+      ${_openjpeg_include_dir}
+      DEPENDS
+      ${_target}
+      ADD_TO_DEPS_LIST
+    )
+  ELSE()
+    RV_ADD_IMPORTED_LIBRARY(
+      NAME
+      OpenJpeg::OpenJpeg
+      TYPE
+      SHARED
+      LOCATION
+      ${_libpath}
+      SONAME
+      ${_libname}
+      INCLUDE_DIRS
+      ${_openjpeg_include_dir}
+      DEPENDS
+      ${_target}
+      ADD_TO_DEPS_LIST
+    )
+  ENDIF()
 ELSE()
-  RV_ADD_IMPORTED_LIBRARY(
-    NAME
-    OpenJpeg::OpenJpeg
-    TYPE
-    SHARED
-    LOCATION
-    ${_libpath}
-    SONAME
-    ${_libname}
-    INCLUDE_DIRS
-    ${_openjpeg_include_dir}
-    DEPENDS
-    ${_target}
-    ADD_TO_DEPS_LIST
-  )
+  # CONFIG creates `openjp2` target. Create `OpenJpeg::OpenJpeg` as an INTERFACE wrapper for backward compatibility (used by oiio.cmake). Only `openjp2` goes in
+  # RV_DEPS_LIST (has LOCATION for install_name_tool -change); OpenJpeg::OpenJpeg is INTERFACE (no LOCATION).
+  IF(NOT TARGET OpenJpeg::OpenJpeg)
+    ADD_LIBRARY(OpenJpeg::OpenJpeg INTERFACE IMPORTED GLOBAL)
+    TARGET_LINK_LIBRARIES(
+      OpenJpeg::OpenJpeg
+      INTERFACE openjp2
+    )
+  ENDIF()
+
+  RV_STAGE_DEPENDENCY_LIBS(TARGET ${_target} TARGET_LIBS openjp2)
 ENDIF()
