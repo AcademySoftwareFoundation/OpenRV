4e53423ed214a6ddffd4c83bd70c1fd93860329e
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2026-04-15 16:38:09 -0400

    build: add build-from-source modules
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

---
+++ cmake/dependencies/build/glew.cmake
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2022  Autodesk, Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Build GLEW from source via ExternalProject_Add (Makefile-based build). Included by cmake/dependencies/glew.cmake when no installed package is found.
+#
+# Expects these variables from the caller (set by RV_CREATE_STANDARD_DEPS_VARIABLES): _target, _version, _install_dir, _lib_dir, _include_dir, _make_command,
+# _cpu_count And these dep-specific variables from the caller: _glew_lib, _glew_lib_name
+
+SET(_download_url
+    "https://github.com/nigels-com/glew/archive/refs/tags/glew-${_version}.tar.gz"
+)
+
+SET(_download_hash
+    ${RV_DEPS_GLEW_DOWNLOAD_HASH}
+)
+
+EXTERNALPROJECT_ADD(
+  ${_target}
+  SOURCE_DIR ${RV_DEPS_BASE_DIR}/${_target}/src
+  INSTALL_DIR ${_install_dir}
+  URL ${_download_url}
+  URL_MD5 ${_download_hash}
+  DOWNLOAD_NAME glew-glew-${_version}.tar.gz
+  DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
+  CONFIGURE_COMMAND cd auto && ${_make_command} && cd .. && ${_make_command}
+  BUILD_COMMAND ${_make_command} -j${_cpu_count} GLEW_DEST=${_install_dir}
+  INSTALL_COMMAND ${_make_command} install LIBDIR=${_lib_dir} GLEW_DEST=${_install_dir}
+  BUILD_IN_SOURCE TRUE
+  BUILD_ALWAYS FALSE
+  BUILD_BYPRODUCTS ${_glew_lib}
+  USES_TERMINAL_BUILD TRUE
+)
+
+RV_ADD_IMPORTED_LIBRARY(
+  NAME
+  GLEW::GLEW
+  TYPE
+  STATIC
+  LOCATION
+  ${_glew_lib}
+  INCLUDE_DIRS
+  ${_include_dir}
+  DEPENDS
+  ${_target}
+  ADD_TO_DEPS_LIST
+)
---
+++ cmake/dependencies/build/openjpeg.cmake
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022  Autodesk, Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Do not build the executables (OpenJPEG calls them "codec executables"). BUILD_THIRDPARTY options is valid only if BUILD_CODEC=ON. PNG, TIFF and ZLIB are not
+# needed anymore because they are used for the executables only.
+LIST(APPEND _configure_options "-DBUILD_CODEC=OFF")
+
+EXTERNALPROJECT_ADD(
+  ${_target}
+  URL ${_download_url}
+  URL_MD5 ${_download_hash}
+  DOWNLOAD_NAME ${_target}_${_version}.tar.gz
+  DOWNLOAD_DIR ${RV_DEPS_DOWNLOAD_DIR}
+  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+  SOURCE_DIR ${_source_dir}
+  BINARY_DIR ${_build_dir}
+  INSTALL_DIR ${_install_dir}
+  DEPENDS ZLIB::ZLIB TIFF::TIFF PNG::PNG
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} ${_configure_options}
+  BUILD_COMMAND ${_cmake_build_command}
+  INSTALL_COMMAND ${_cmake_install_command}
+  BUILD_IN_SOURCE FALSE
+  BUILD_ALWAYS FALSE
+  BUILD_BYPRODUCTS ${_byproducts}
+  USES_TERMINAL_BUILD TRUE
+)
