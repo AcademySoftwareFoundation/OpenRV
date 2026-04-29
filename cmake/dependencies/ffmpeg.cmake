e3077e961a47add987e4025089cf166d00989df5
Michael Oliver <mcoliver@gmail.com>
2026-04-29 16:11:53 -0700

    Prioritize Homebrew's ffmpeg-full pkg-config path over standard ffmpeg
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/dependencies/ffmpeg.cmake
+++ cmake/dependencies/ffmpeg.cmake
@@ -19,48 +19,101 @@
 # ------------------------------------------------------------------------------
 
 IF(RV_USE_SYSTEM_DEPS)
+  # Prefer ffmpeg-full from Homebrew if it exists, as it is keg-only and contains all the required non-free codecs (like ProRes).
+  IF(APPLE)
+    IF(EXISTS "/opt/homebrew/opt/ffmpeg-full/lib/pkgconfig")
+      SET(ENV{PKG_CONFIG_PATH}
+          "/opt/homebrew/opt/ffmpeg-full/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}"
+      )
+    ELSEIF(EXISTS "/usr/local/opt/ffmpeg-full/lib/pkgconfig")
+      SET(ENV{PKG_CONFIG_PATH}
+          "/usr/local/opt/ffmpeg-full/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}"
+      )
+    ENDIF()
+  ENDIF()
+
   FIND_PACKAGE(PkgConfig REQUIRED)
-  
+
   # Individual check for each module to get specific variables
-  FOREACH(_lib avcodec avformat avutil swresample swscale)
+  FOREACH(
+    _lib
+    avcodec avformat avutil swresample swscale
+  )
     PKG_CHECK_MODULES(FFMPEG_lib${_lib} REQUIRED lib${_lib})
-    
+
     IF(NOT TARGET ffmpeg::${_lib})
       ADD_LIBRARY(ffmpeg::${_lib} INTERFACE IMPORTED GLOBAL)
-      
+
       # Try to find the actual library file to avoid "library not found" errors
-      FIND_LIBRARY(FFMPEG_lib${_lib}_PATH NAMES ${_lib} PATHS ${FFMPEG_lib${_lib}_LIBRARY_DIRS} ${FFMPEG_lib${_lib}_LIBDIR})
-      
+      FIND_LIBRARY(
+        FFMPEG_lib${_lib}_PATH
+        NAMES ${_lib}
+        PATHS ${FFMPEG_lib${_lib}_LIBRARY_DIRS} ${FFMPEG_lib${_lib}_LIBDIR}
+      )
+
       IF(FFMPEG_lib${_lib}_PATH)
-        SET_PROPERTY(TARGET ffmpeg::${_lib} PROPERTY INTERFACE_LINK_LIBRARIES "${FFMPEG_lib${_lib}_PATH}")
+        SET_PROPERTY(
+          TARGET ffmpeg::${_lib}
+          PROPERTY INTERFACE_LINK_LIBRARIES "${FFMPEG_lib${_lib}_PATH}"
+        )
       ELSE()
         # Fallback to the names if path not found
-        SET_PROPERTY(TARGET ffmpeg::${_lib} PROPERTY INTERFACE_LINK_LIBRARIES "${FFMPEG_lib${_lib}_LIBRARIES}")
+        SET_PROPERTY(
+          TARGET ffmpeg::${_lib}
+          PROPERTY INTERFACE_LINK_LIBRARIES "${FFMPEG_lib${_lib}_LIBRARIES}"
+        )
       ENDIF()
-      
-      SET_PROPERTY(TARGET ffmpeg::${_lib} PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${FFMPEG_lib${_lib}_INCLUDE_DIRS}")
-      SET_PROPERTY(TARGET ffmpeg::${_lib} PROPERTY INTERFACE_COMPILE_OPTIONS "${FFMPEG_lib${_lib}_CFLAGS_OTHER}")
-      
+
+      SET_PROPERTY(
+        TARGET ffmpeg::${_lib}
+        PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${FFMPEG_lib${_lib}_INCLUDE_DIRS}"
+      )
+      SET_PROPERTY(
+        TARGET ffmpeg::${_lib}
+        PROPERTY INTERFACE_COMPILE_OPTIONS "${FFMPEG_lib${_lib}_CFLAGS_OTHER}"
+      )
+
       # Promote to global if it came from PkgConfig target
       IF(TARGET PkgConfig::FFMPEG_lib${_lib})
-         SET_PROPERTY(TARGET PkgConfig::FFMPEG_lib${_lib} PROPERTY IMPORTED_GLOBAL TRUE)
-         # Prefer the PkgConfig target as it handles internal dependencies better
-         SET_PROPERTY(TARGET ffmpeg::${_lib} PROPERTY INTERFACE_LINK_LIBRARIES PkgConfig::FFMPEG_lib${_lib})
+        SET_PROPERTY(
+          TARGET PkgConfig::FFMPEG_lib${_lib}
+          PROPERTY IMPORTED_GLOBAL TRUE
+        )
+        # Prefer the PkgConfig target as it handles internal dependencies better
+        SET_PROPERTY(
+          TARGET ffmpeg::${_lib}
+          PROPERTY INTERFACE_LINK_LIBRARIES PkgConfig::FFMPEG_lib${_lib}
+        )
       ENDIF()
     ENDIF()
   ENDFOREACH()
-  
+
   # Ensure cross-linking between components
-  TARGET_LINK_LIBRARIES(ffmpeg::avcodec INTERFACE ffmpeg::avutil ffmpeg::swresample)
-  TARGET_LINK_LIBRARIES(ffmpeg::avformat INTERFACE ffmpeg::avcodec)
-  TARGET_LINK_LIBRARIES(ffmpeg::swscale INTERFACE ffmpeg::avutil)
-  TARGET_LINK_LIBRARIES(ffmpeg::swresample INTERFACE ffmpeg::avutil)
-  
+  TARGET_LINK_LIBRARIES(
+    ffmpeg::avcodec
+    INTERFACE ffmpeg::avutil ffmpeg::swresample
+  )
+  TARGET_LINK_LIBRARIES(
+    ffmpeg::avformat
+    INTERFACE ffmpeg::avcodec
+  )
+  TARGET_LINK_LIBRARIES(
+    ffmpeg::swscale
+    INTERFACE ffmpeg::avutil
+  )
+  TARGET_LINK_LIBRARIES(
+    ffmpeg::swresample
+    INTERFACE ffmpeg::avutil
+  )
+
   # Add OpenSSL dependency to avutil as many FFmpeg builds require it
   FIND_PACKAGE(OpenSSL QUIET)
-  if (TARGET OpenSSL::Crypto)
-    TARGET_LINK_LIBRARIES(ffmpeg::avutil INTERFACE OpenSSL::Crypto)
-  endif()
+  IF(TARGET OpenSSL::Crypto)
+    TARGET_LINK_LIBRARIES(
+      ffmpeg::avutil
+      INTERFACE OpenSSL::Crypto
+    )
+  ENDIF()
 
   RETURN()
 ENDIF()
@@ -261,11 +314,13 @@ ENDIF()
 
 OPTION(RV_FFMPEG_USE_VIDEOTOOLBOX "FFmpeg laveraging the VideoToolbox framework" ${RV_FFMPEG_USE_VIDEOTOOLBOX_DEFAULT_VALUE})
 
-# Make a list of the Visto's FFmpeg config options unless already customized. Note that a super project, a project consuming Visto as a submodule, can
-# customize the FFmpeg config options via the RV_FFMPEG_CONFIG_OPTIONS cmake property.
+# Make a list of the Visto's FFmpeg config options unless already customized. Note that a super project, a project consuming Visto as a submodule, can customize
+# the FFmpeg config options via the RV_FFMPEG_CONFIG_OPTIONS cmake property.
 IF(NOT RV_FFMPEG_CONFIG_OPTIONS)
-  SET(RV_FFMPEG_CONFIG_OPTIONS "")
-  
+  SET(RV_FFMPEG_CONFIG_OPTIONS
+      ""
+  )
+
   IF(NOT RV_FFMPEG_CONFIG_OPTIONS STREQUAL RV_FFMPEG_CONFIG_OPTIONS_CACHE)
     SET(${_force_rebuild}
         TRUE
